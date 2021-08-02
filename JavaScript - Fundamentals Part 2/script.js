5af222a924d69ad93794157da0e306a5da55a4a5
Fabian Garcia <fabianarielgarcia@gmail.com>
2021-08-02 20:06:44 -0300

    Update script.js
    Basic array operations (methods).  Coding Challenge #2. Introduction to Objects. Dot vs. Bracket Operations.

--- JavaScript - Fundamentals Part 2/script.js
+++ JavaScript - Fundamentals Part 2/script.js
@@ -1,6 +1,8 @@
 'use strict';
 /*
 ///Funciones
+
+/*
 function describeCountry(country, population, capitalCity) {
     let pais = `${country} has ${population} millon people and its capital city is ${capitalCity}.`;
     return pais;
@@ -14,9 +16,14 @@ console.log(Finald);
 console.log(Argentina);
 console.log(Brasil);
 
-
-//FUNCTION DECLARATIONS AND FUNCTION EXPRESSIONS
 */
+//FUNCTION DECLARATIONS AND FUNCTION EXPRESSIONS
+<<<<<<< Updated upstream
+*/
+=======
+
+/*
+>>>>>>> Stashed changes
 function percentageOfWorld(population) {
     const worldPopulation = 7900000000;
 
@@ -48,8 +55,10 @@ let china2 = percentageOfWorld2("China", 1441000000);
 
 console.log(china2);
 
+*/
 //ARROW FUNTIONS
 
+/*
 let percentageOfWorld3 = (country, population) => {
     const worldPopulation = 7900000000;
     let avgPopulation = (population * 100) / worldPopulation;
@@ -60,16 +69,24 @@ let percentageOfWorld3 = (country, population) => {
 
 console.log(percentageOfWorld3("China", 1441000000));
 
+*/
 
 ///FUNCTIONS CALLING OTHER FUNTIONS
+
+/*
 function describePopulaton(country, population) {
     let avg = percentageOfWorld(population);
     return `${country} has ${population} millon people, which is about ${avg} of the world population.`;
 }
 
 console.log(describePopulaton("China", 1441000000));
+<<<<<<< Updated upstream
 */
 
+=======
+
+*/
+>>>>>>> Stashed changes
 ///COODING CHALLENGE #1 JavaScript Fundamentals – Part 2
 /*
 Your tasks:
@@ -119,6 +136,8 @@ checkWinner(dolphinsScoreAvg, koalasScoreAvg);
 */
 
 ///ARRAYS
+
+/*
 const population = [10000000, 20000000, 30000000, 40000000];
 
 if (population.length === 4) {
@@ -130,4 +149,176 @@ if (population.length === 4) {
 const percentage = [percentageOfWorld(population[1])];
 console.log(percentage);
 
+<<<<<<< Updated upstream
 console.log('Hellow world');
+=======
+
+*/
+/*
+LECTURE: Basic Array Operations (Methods)
+1. Create an array containing all the neighbouring countries of a country of your
+choice. Choose a country which has at least 2 or 3 neighbours. Store the array
+into a variable called 'neighbours'
+2. At some point, a new country called 'Utopia' is created in the neighbourhood of
+your selected country. So add it to the end of the 'neighbours' array
+3. Unfortunately, after some time, the new country is dissolved. So remove it from
+the end of the array
+4. If the 'neighbours' array does not include the country ‘Germany’, log to the
+console: 'Probably not a central European country :D'
+5. Change the name of one of your neighbouring countries. To do that, find the
+index of the country in the 'neighbours' array, and then use that index to
+change the array at that index position. For example, you can search for
+'Sweden' in the array, and then replace it with 'Republic of Sweden'.
+
+//1
+const neighbours = ['Brasil', 'Uruguay', 'Chile', 'Paraguay', 'Bolivia'];
+
+console.log(neighbours);///testing the array
+//2
+neighbours.push('Utopia');
+
+console.log(neighbours);///array with the new country
+//3
+neighbours.pop();
+
+console.log(neighbours);
+//4
+const germany = neighbours.includes('Germany');
+
+if (!germany) {
+    console.log(`Probably not a central European country :D`);
+}
+
+//5
+const positionCountry = neighbours.indexOf('Paraguay');
+
+console.log(positionCountry);
+
+neighbours[positionCountry] = 'Utopia';
+
+console.log(neighbours);
+
+*/
+
+/*
+Coding Challenge #2
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+20%.
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns
+the corresponding tip, calculated based on the rules above (you can check out
+the code from first tip calculator challenge if you need to). Use the function
+type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data
+below
+3. Create an array 'tips' containing the tip value for each bill, calculated from
+the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+Test data: 125, 555 and 44
+Hint: Remember that an array needs a value in each position, and that value can
+actually be the returned value of a function! So you can just call a function as array
+values (so don't store the tip values in separate variables first, but right in the new
+array) 😉
+GOOD LUCK 😀
+*/
+/*MINE
+//1
+const bill = Number(prompt('Total bill: '));//Use prompt so I do not have to hardcodear the data. I have not learnt exceptions yet.
+function calcTip(bill) {
+
+    let tip = 0;
+
+    if (bill >= 50 && bill <= 300) {
+        tip = bill * 0.15;
+    } else {
+        tip = bill * 0.2;
+    }
+    return tip;
+}
+
+//2
+let bills = [];
+bills.push(bill);
+console.log(bills);
+
+//3
+let tips = [];
+tips.push(calcTip(bill));
+console.log(tips);
+
+//4
+let total = [];
+total.push(bills[0] + tips[0]);
+console.log(total);
+*/
+/*
+//TEACHER´S RESOLUTION
+let calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+let bills = [125, 555, 44];
+
+let tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+let totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
+*/
+
+//LECTURE: Introduction to Objects
+/*
+1. Create an object called 'myCountry' for a country of your choice, containing
+properties 'country', 'capital', 'language', 'population' and
+'neighbours' (an array like we used in previous assignments)
+
+let myCountry = {
+    country: 'Argentina',
+    capital: 'Buenos Aires',
+    language: 'Spanish',
+    population: 44000000,
+    neighbours: ['Brasil', 'Uruguay', 'Chile', 'Paraguay', 'Bolivia']
+};
+
+console.log(myCountry);
+*/
+
+//Challenge
+//Write this sentence: "Fabian tiene 3 amigos, y  su mejor amigo es Erik."
+/*
+const persona = {
+    firstName: 'Fabian',
+    lastName: 'Garcia',
+    edad: 32,
+    profesion: 'comerciante',
+    amigos: ['Erik', 'Fede', 'Rodri']
+};
+
+console.log(`${persona.firstName} has ${persona.amigos.length} friends, and his best friend is ${persona.amigos[0]}.`);
+*/
+/*
+LECTURE: Dot vs. Bracket Notation
+1. Using the object from the previous assignment, log a string like this to the
+console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries
+and a capital called Helsinki.'
+2. Increase the country's population by two million using dot notation, and then
+decrease it by two million using brackets notation.
+
+let myCountry = {
+    country: 'Argentina',
+    capital: 'Buenos Aires',
+    language: 'Spanish',
+    population: 44000000,
+    neighbours: ['Brasil', 'Uruguay', 'Chile', 'Paraguay', 'Bolivia']
+};
+
+console.log(`{${myCountry.country} has ${myCountry.population} millon ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neibours countries and a capital called ${myCountry.capital}.`);
+
+myCountry.population = 42000000;
+console.log(myCountry);
+
+myCountry['population'] = 44000000;
+console.log(myCountry);
+*/
+>>>>>>> Stashed changes
