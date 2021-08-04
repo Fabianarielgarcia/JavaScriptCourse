902b52a1c34486c9d82ba97b4196463478e43c60
Fabian Garcia <fabianarielgarcia@gmail.com>
2021-08-04 18:21:34 -0300

    Update script.js
    Object Methods. Coding challenge #3.

--- JavaScript - Fundamentals Part 2/script.js
+++ JavaScript - Fundamentals Part 2/script.js
@@ -18,12 +18,7 @@ console.log(Brasil);
 
 */
 //FUNCTION DECLARATIONS AND FUNCTION EXPRESSIONS
-<<<<<<< Updated upstream
-*/
-=======
 
-/*
->>>>>>> Stashed changes
 function percentageOfWorld(population) {
     const worldPopulation = 7900000000;
 
@@ -80,13 +75,7 @@ function describePopulaton(country, population) {
 }
 
 console.log(describePopulaton("China", 1441000000));
-<<<<<<< Updated upstream
-*/
 
-=======
-
-*/
->>>>>>> Stashed changes
 ///COODING CHALLENGE #1 JavaScript Fundamentals – Part 2
 /*
 Your tasks:
@@ -149,9 +138,6 @@ if (population.length === 4) {
 const percentage = [percentageOfWorld(population[1])];
 console.log(percentage);
 
-<<<<<<< Updated upstream
-console.log('Hellow world');
-=======
 
 */
 /*
@@ -321,4 +307,85 @@ console.log(myCountry);
 myCountry['population'] = 44000000;
 console.log(myCountry);
 */
->>>>>>> Stashed changes
+
+/*
+LECTURE: Object Methods
+1. Add a method called 'describe' to the 'myCountry' object. This method
+will log a string to the console, similar to the string logged in the previous
+assignment, but this time using the 'this' keyword.
+2. Call the 'describe' method
+3. Add a method called 'checkIsland' to the 'myCountry' object. This
+method will set a new property on the object, called 'isIsland'.
+'isIsland' will be true if there are no neighbouring countries, and false if
+there are. Use the ternary operator to set the property.
+
+
+let myCountry = {
+    country: 'Argentina',
+    capital: 'Buenos Aires',
+    language: 'Spanish',
+    population: 44000000,
+    neighbours: ['Brasil', 'Uruguay', 'Chile', 'Paraguay', 'Bolivia'],
+    describe: function () {
+        const message = `${this.country}' capital is ${this.capital}. There are ${this.population} millon people living in it. They speak ${this.language}.`;
+        return message;
+    },
+    checkIsIsland: function () {
+        return this.neighbours.length > 0 ? this.isIsland = false : this.isIsland = true;
+    }
+};
+console.log(myCountry.describe());
+myCountry.checkIsIsland();
+console.log(myCountry);
+*/
+/*
+Coding Challenge #3
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+implement the calculations! Remember: BMI = mass / height ** 2 = mass
+/ (height * height) (mass in kg and height in meter)
+Your tasks:
+1. For each of them, create an object with properties for their full name, mass, and
+height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+method on both objects). Store the BMI value to a property, and also return it
+from the method
+3. Log to the console who has the higher BMI, together with the full name and the
+respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+tall.
+GOOD LUCK 😀
+*/
+
+const mark = {
+    firstName: 'Mark',
+    lastName: 'Dell',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height ** 2);
+        return this.bmi;
+    }
+};
+
+const john = {
+    firstName: 'John',
+    lastName: 'Wright',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height ** 2);
+        return this.bmi;
+    }
+};
+
+const bmiMark = mark.calcBMI();
+const bmiJohn = john.calcBMI();
+
+console.log(mark);
+console.log(john);
+
+if (bmiMark > bmiJohn) {
+    console.log(`${mark.firstName}'s BMI (${bmiMark}) is haigher than John's (${bmiJohn})!`);
+} else {
+    console.log(`${john.firstName}'s BMI (${bmiJohn}) is haigher than Mark's (${bmiMark})!`);
+}
