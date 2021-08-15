4cc765e2b7733c0157fb40eec314eb074c2f6374
Fabian Garcia <fabianarielgarcia@gmail.com>
2021-08-15 13:40:17 -0300

    Handling  a keypress event
    handling an "Esc" keypress event. Getting the information about which key was pressed.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,16 +6,15 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
-
 //Function to open the modal
-const openModal = function(){
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-}
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 //EventHandeler to each button
-for (let i = 0; i < btnsOpenModal.length; i++) {
+for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
-    
+
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
@@ -25,3 +24,11 @@ btnCloseModal.addEventListener('click', closeModal);
 
 //function to close the model when I click on the overlay (without clicking the "X" button)
 overlay.addEventListener('click', closeModal);
+
+///Handeling a key press event
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
