cfe2067d5a95d379b69b03ce69053a6c32ad45ff
Fabian Garcia <fabianarielgarcia@gmail.com>
2021-08-25 17:16:46 -0300

    Update script.js
    Destructuring objects.  Mutating variables.  Nested objects.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -8,13 +8,66 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 };
 
-///ARRAY DESTRUCTURING (UNPACKING VALUES INTO SEPARATE VARIABLES)
+///DESTRUCTURING OBJECTS
+//To destructure an object we use "{}". Then we provide the variable names that exactly match the property names that we want to retrieve from the object. We do not have to forget that in an ovject the order of the properties do not matter
 
+const { name, openingHours, categories } = restaurant;
+//console.log(name, openingHours, categories);
+
+///What if we want the variable names to be different from the properties names?
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+//console.log(restaurantName, hours, tags);
+///DEFAULT VALUES
+///We can have default values for examples when we are trying to read a property that does not exist on the object. This can be useful wen we do not work with hard-coded data
+const { menu = [], starterMenu: starters = [] } = restaurant;
+
+console.log(menu, starters);
+
+///MUTATING VARIABLES
+///mutating variables while destructuring objects
+
+let a = 111; ///should become 23
+let b = 999; ///should become 7
+
+const obj = { a: 23, b: 7, c: 14 };
+
+///{a, b} = obj; Error because JavaScript expects a block of code when we start a new line with "{}"
+
+({ a, b } = obj);
+console.log(a, b);
+
+///NESTED OBJECTS
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close); ///output --> 11 23
+
+///ARRAY DESTRUCTURING (UNPACKING VALUES INTO SEPARATE VARIABLES)
+/*
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -26,10 +79,10 @@ console.log(x, y, z);
 console.log(arr);
 
 ///working with the restaurant
-/*
+
 const [first, second] = restaurant.categories;
 console.log(first, second);
-*/
+
 ///what if we want to destruct the first and the third element?
 const [first, , third] = restaurant.categories;
 console.log(first, third);
@@ -64,4 +117,5 @@ console.log(p, q, r); ///we get r = undefined because one of the arrays have onl
 
 ///this can be useful for example when we get data from an API
 const [c = 1, d = 1, f = 1] = [8, 9];
-console.log(c, d, f); ///we get r = undefined because one of
+console.log(c, d, f); ///we get c=8, d= 9, f=1
+*/
