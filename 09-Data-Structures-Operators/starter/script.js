991e03092c6b4ace10cea90e240a94fe8ebc9e84
Fabian Garcia <fabianarielgarcia@gmail.com>
2021-08-27 20:21:54 -0300

    Update script.js
    Coding challenge 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Data needed for first part of the section
+/*
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -33,10 +33,10 @@ const restaurant = {
     );
   },
 };
-
-<<<<<<< Updated upstream
-=======
+*/
 /**We can use the && and || operators for short circuiting. Logical operators can use any data type and they can return any data type. In the case of the or operator, short-circuiting means that if the first value is a trurhy value, then the other operand will not even be avaluated and it will immediately return that first value.*/
+
+/*
 console.log('----OR-->SHORT-CIRCUITING-----');
 console.log(33 || 'Fabian');
 console.log('' || 'Fabian');
@@ -44,7 +44,11 @@ console.log(true || '0');
 console.log(undefined || null);
 console.log(undefined || 0 || '' || 'Hello' || 23 || null);
 
+
+*/
 /**When it comes to short-circuiting, the AND operator works in the opposite way of the or operator. The and operator short circuits when the first value is falsy. Then immediately returns that falsy value without even evaluating the second operand*/
+
+/*
 console.log('----&&-->SHORT-CIRCUITING-----');
 
 console.log(0 && 'Fabian');
@@ -58,6 +62,8 @@ if (restaurant.orderPizza) {
 
 restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
+
+*/
 ///REST PATTERNS AND REST PARAMETERS
 /** The rest patter has the same look as the spread operator but it does exactly the opposite. The rest pattern is used to collect multiple elements and condense them into an array*/
 /*
@@ -85,7 +91,6 @@ restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
 restaurant.orderPizza('mushrooms');
 */
 
->>>>>>> Stashed changes
 ///THE SPREAD OPERATOR
 ///We can use the spread operator to basically expand an array into all its elements
 
@@ -98,13 +103,19 @@ console.log(newArr);
 */
 
 ///Using the spread operator
+
+/*
 const newArr = [1, 2, ...arr];
 console.log(newArr); ///---> [1, 2 ,7, 8 ,9]
 console.log(...newArr); ///it logs the individual elements of the array
 
+
+
+*/
 /***THE BIG DIFFERENCE BETWEEN THE SPREAD OPERATOR AND DESTRUCTURING IS THAT THE SPREAD OPERATOR TAKES ALL THE ELEMENTS FROM THE ARRAY AND IT ALSO DOES NOT CREATE NEW VARIABLES. AS A CONSEQUENCE, WE CAN ONLY USE IT IN PLACES WHERE WE WOULD OTHERWISE WRITE VALUES SEPARATED BY COMMAS.
  */
 
+/*
 ///1) Copy array.
 const mainMenuCopy = [...restaurant.mainMenu];
 console.log(mainMenuCopy);
@@ -148,6 +159,8 @@ restaurantCopy.name = 'La Piazzeta';
 
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
+
+*/
 /*If we do this
 const newArr = [1, 2, arr];
 console.log(newArr); ---> [1, 2, Array(3)]
@@ -242,3 +255,100 @@ console.log(p, q, r); ///we get r = undefined because one of the arrays have onl
 const [c = 1, d = 1, f = 1] = [8, 9];
 console.log(c, d, f); ///we get c=8, d= 9, f=1
 */
+
+/***We're building a football betting app (soccer for my American friends 😅)!
+Suppose we get data from a web service about a certain game ('game' variable on
+next page). In this challenge we're gonna work with that data.
+Your tasks:
+1. Create one player array for each team (variables 'players1' and
+'players2')
+2. The first player in any player array is the goalkeeper and the others are field
+players. For Bayern Munich (team 1) create one variable ('gk') with the
+goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+field players
+3. Create an array 'allPlayers' containing all players of both teams (22
+players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+new array ('players1Final') containing all the original team1 players plus
+'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called
+'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player
+names (not an array) and prints each of them to the console, along with the
+number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which
+team is more likely to win, without using an if/else statement or the ternary
+operator.
+Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+Then, call the function again with players from game.scored
+GOOD LUCK 😀 */
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+///1
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+
+///2
+const [gk, ...fieldPlayers] = game.players[0];
+console.log(gk);
+console.log(fieldPlayers);
+
+///3
+const allPlayers = [...game.players[0], ...game.players[1]];
+console.log(allPlayers);
+
+///4
+const players1Final = [...game.players[0], 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+///5
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+
+///6
+const printGoals = function (players) {
+  console.log(players);
+};
+
+printGoals(...game.score);
