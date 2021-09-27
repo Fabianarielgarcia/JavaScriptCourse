40c7d98fe62056d42e5460d939595aed0d909ff3
Fabian Garcia <fabianarielgarcia@gmail.com>
2021-09-27 16:45:32 -0300

    Update script.js
    First-class functions and higer-order functions (theory).

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -20,6 +20,7 @@ createBooking('LH123');
 createBooking('LH123', 2, 500);
 createBooking('Lh123', undefined, 20); ///to skip a default parameter
 */
+/*
 ////HOW PASSING ARGUMENTS WORKS: VALUE VS. REFERENCE
 
 const flight = 'LH123'; ///primitive type
@@ -46,3 +47,8 @@ console.log(fabian); ///output--->Mr Fabian
 ///WHEN WE PASS A REFERENCE TYPE TO A FUNCTION, WHAT IS COPIED IS REALLY JUST A REFERENCE TO THE OBJECT IN THE MEMORY HEAP (DOING THIS flightNum = flight). WHEN WE TRY TO COPY AN OBJECT LIKE THIS (const passenger = fabian) WE ARE REALLY ONLY COPYING THE REFERENCE TO THAT OBJECT IN THE MEMORY HEAP, BUT THEY BOTH POINT TO THE SAME OBJECT IN MEMORY. THAT IS WHY, WHEN WE PASS THE OBJECT AS AN ARGUMENT IN THE FUNCTION IT IS EXACTLY THE SAME AS MANIPULATING THE FABIAN OBJECT BECAUSE BOTH ARE THE SAME OBJECT IN THE MEMORY HEAP.
 
 ///PASSING BY VALUE AND PASSING BY REFERENCE. JAVASCRIPT DOES NOT HAVE PASSING BY REFERENCE. fOR OBJECTS, WE DO IN FACT PASS IN A REFERENCE, HOWEVER THAT REFERENCE ITSELF IS STILL A VALUE THAT CONTAINS A MEMORY ADDRESS. BASICALLY WE PASS A REFERENCE TO THE FUNCTION, BUT WE DO NOT PASS BY REFERENCE
+*/
+
+///FIRST-CLASS AND HIGHER-ORDER FUNCTIONS
+///JavaScript is a language that has first class functions which in technical terms means that functions are so-called first citizens. In practice, that means that functions are simply treated as values. Since functions are values, there is a bunch of interesting things that we can do with them, like storing them in variables or objects properties, passing functions as arguments in other functions or return a function from another function. As functions are objects, there are also methods that we can call on functions.
+///A higher order function is either a function that receives another function as an argument (callback function), or a function that returns a new function.
